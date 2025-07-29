a103bc6835f75f406a985cd52ecc6f370baf0d1b
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-07-29 05:06:04 -0700

    demonstration of using javascript function

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer(){
     return (
         <footer>
-            <h1>&copy; This is footer for my website</h1>
+            <h1>&copy; {new Date().getFullYear()} This is footer for my website</h1>
         </footer>
     );
 }
