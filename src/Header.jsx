541e82bbb83197920cfe6fb8b61650c0279cfa4d
raiyan12999 <raiyanbinatik.m29@gmail.com>
2025-07-29 05:01:41 -0700

    reused food component

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,15 @@ function Header(){
     return (
         <header>
             <h1>My Website</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Service</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
         </header>
     );
 }
